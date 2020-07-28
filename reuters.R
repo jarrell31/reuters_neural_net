30137f683589b0b35e7c52254ca2bd90e186f40e
jarrell31 <68021794+jarrell31@users.noreply.github.com>
2020-07-28 16:51:46 -0400

    Update reuters.R

--- reuters.R
+++ reuters.R
@@ -1,5 +1,3 @@
-setwd("C:/Users/Owner/Desktop/The 9 Projects/Reuters")
-
 library(keras)
 reuters <- dataset_reuters(num_words = 10000)
 c(c(train_data, train_labels), c(test_data, test_labels)) %<-% reuters
